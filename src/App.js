07c0ce8d6960f7e146d12d5768f6e283880e0864
anjaliprt <anjalir303@gmail.com>
2024-08-20 19:25:25 +0400

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { Component } from 'react';
 
-
-
-function App() {
+class App extends Component{
+render(){
   return (
-    <div >
-      
-    
+    <div className="header">
+    {/* <div >
+      Phone Directory
+    </div>
+    <button>Add</button>  
+    <div>
+      <span> Name </span><br/>
+      <span> Phone </span>
+    </div> */}
+    <label htmlFor="name">Name:</label>
+    <input id = "name" type="text" placeholder ="Type Here" defaultValue="Anjali"/>
     </div>
   );
+}
 }
 
 export default App;
